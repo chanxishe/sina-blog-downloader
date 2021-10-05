f174efe5cb80eae77d7c642aa45388a571ba3bfb
Gavin Fu <gavfu@outlook.com>
2021-10-06 00:11:31 +0800

    Get blog uid

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,40 @@
 require('dotenv').config();
 
 import init from './init';
+import { SINA_ARTICLE_LIST_URL_PREFIX, SINA_BLOG_URL_PREFIX } from './constants';
+
+const _ = require('lodash');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 init();
 
+async function getUid() {
+  const blogUrl = process.env.SINA_BLOG_URL_TO_DOWNLOAD;
+  if (_.isEmpty(blogUrl)) {
+    throw new Error('No blog to download');
+  }
+
+  const dom = await JSDOM.fromURL(blogUrl, {
+    runScripts: "dangerously"
+  });
+
+  // Example element: <meta http-equiv="mobile-agent" content="format=html5; url=http://blog.sina.cn/dpool/blog/u/1215172700">
+  const metaList = dom.window.document.querySelectorAll('meta');
+  const meta = _.find(metaList, (m: any) => new RegExp('url=http://blog\.sina\.cn/dpool/blog/u/(\\w+)').test(m.content));
+  if (_.isUndefined(meta)) {
+    throw new Error('Failed to find uid');
+  }
+  return meta.content.match('url=http://blog\.sina\.cn/dpool/blog/u/(\\w+)')[1];
+}
+
+
 async function main() {
-  console.log('Hello world');
+  // Get uid
+  const uid = await getUid();
+  console.log(uid);
+
+
 }
 
 main();
---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+
+export const SINA_BLOG_URL = 'http://blog.sina.com.cn/';
+
+export const SINA_ARTICLE_LIST_URL_PREFIX = `${SINA_BLOG_URL}s/articlelist_`;
+
+export const SINA_BLOG_URL_PREFIX = `${SINA_BLOG_URL}s/blog_`;
